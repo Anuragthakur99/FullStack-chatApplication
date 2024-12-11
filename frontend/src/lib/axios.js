9ac8defc46b9cd375a5fecbf3560c917010db4d7
Anuragthakur99 <anurag.thkr009@gmail.com>
2024-12-12 05:14:18 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" 
     ? "http://localhost:5001/api" 
-    : "https://backendforwebtalk.onrender.com/api",
+    : "https://fullstack-chatapplication-j28m.onrender.com/api",
   withCredentials: true,
 });
